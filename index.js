7db8d91b533c40ba087da92b2f741c78df14dbee
tiangoucoin <chenyongren1988@gmail.com>
2021-05-12 02:26:00 +0800

    v1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     let code = generateVerify(4);
+    let shareCode = generateVerify(18);
     $('#code').text(code);
+    $('#share_link').val(`https://tiangoucoin.github.io/tiangoucoin?invite=${shareCode}`);
 
     $('#get').click(function() {
         const address = $('#address').val();
@@ -13,10 +15,50 @@ $(document).ready(function() {
             alert('请输入正确的验证码');
             return;
         }
-        console.log(address, verify)
+        $('#verify_box').hide();
+        $('#verified_box').show();
     });
 
-    $('#coin_select').click(function() {
-        $('.coin-dropdown').toggle();
+    $('#select').click(function() {
+        $('.select-dropdown').toggle();
+    });
+
+    const from = $('#from');
+    const to = $('#to');
+    const intReg = /^[0-9]*$/;
+    const buyFrom = $('#buy_from');
+    const buyTo = $('#buy_to');
+    const buyUnit = $('#buy_unit');
+
+    $('#select .select-dropdown-item').click(function(item) {
+        const value = $(this).text();
+        $('#unit_value').text(value);
+        from.val(0.0);
+        to.val(0.0);
+    });
+
+
+    from.on('input', function (e) {
+        let value = $(this).val();
+        const unit = $('#unit_value').text();
+        const fromUnit = unit === 'USDT' ? 100000 : 400000000;
+        if (!intReg.test(value)) {
+            from.val(value.slice(0, -1));
+        }
+        to.val(value * fromUnit);
+    });
+    
+    $('#buy').on('click', function() {
+        if (!from.val()) {
+            alert('请输入购买数量');
+            return;
+        }
+        $('.mask').show();
+        buyFrom.text(from.val());
+        buyTo.text(to.val());
+        buyUnit.text($('#unit_value').text());
+    });
+    $('.mask-bg').on('click', function() {
+        $('.mask').hide();
     });
 });
--- utils.js
+++ utils.js
@@ -6,3 +6,10 @@ function generateVerify(length) {
     }
     return verify;
 }
+
+function copyContent(id){
+    var copyCon = document.getElementById(id);
+    copyCon.select(); // 选择对象
+    document.execCommand("Copy"); // 执行浏览器复制命令
+    alert('复制成功');
+}
